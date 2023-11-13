f15254373dbdac6002297cf18f92cbfda7e02ea3
kim-jingyu <swc05161@gmail.com>
2023-11-13 12:25:19 +0000

    fix: realloc

--- mm.c
+++ mm.c
@@ -135,22 +135,35 @@ void mm_free(void *bp)
     coalesce(bp);
 }
 
-void *mm_realloc(void *ptr, size_t size)
+void *mm_realloc(void *bp, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
+    size_t old_size = GET_SIZE(HDRP(bp));
+    size_t new_size = size + (2 * WSIZE);
 
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-        return NULL;
-    
-    copySize = GET_SIZE(HDRP(oldptr));
-    if (size < copySize)
-        copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+    // new_size가 old_size보다 작거나 같으면 기존 bp 그대로 사용
+    if (new_size <= old_size) {
+        return bp;
+    }
+    // new_size가 old_size보다 크면 사이즈 변경
+    else {
+        size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+        size_t current_size = old_size + GET_SIZE(HDRP(NEXT_BLKP(bp)));
+
+        // next block이 free 상태이고 old, next block의 사이즈 합이 new_size보다 크면, next block에 합쳐서 사용
+        if (!next_alloc && current_size >= new_size) {
+            PUT(HDRP(bp), PACK(current_size, 1));
+            PUT(FTRP(bp), PACK(current_size, 1));
+            return bp;
+        }
+        // 아니면 새로 block 만들어서 거기로 옮기기
+        else {
+            void *new_bp = mm_malloc(new_size);
+            place(new_bp, new_size);
+            memcpy(new_bp, bp, new_size);  // old_bp로부터 new_size만큼의 문자를 new_bp로 복사하는 함수
+            mm_free(bp);
+            return new_bp;
+        }
+    }
 }
 
 static void *extend_heap(size_t words)
