84a497054339427ba6027ef11b94a8aedca88c2e
kim-jingyu <swc05161@gmail.com>
2023-11-22 14:05:33 +0000

    feat: main, thread

--- proxy.c
+++ proxy.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+void *thread(void *vargp);
+void doit(int clientfd);
+void read_requsethdrs(rio_t *rp, void *buf, int serverfd, char *hostname, char *port);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void parse_uri(char *uri, char *hostname, char *port, char *path);
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
+/* 테스트 환경에 따른 도메인,포트 번호 지정을 위한 상수이다. (0 할당시에 도메인,포트 번호가 고정되어서 외부에서 접속이 가능하다.) */
+static const int in_local_test = 1;
 
-int main() {
-  printf("%s", user_agent_hdr);
+int main(int argc, char **argv)
+{
+  int listenfd, *clientfd;
+  char client_hostname[MAXLINE], client_port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+  pthread_t tid;
+  signal(SIGPIPE, SIG_IGN);
+
+  // rootp = (web_object_t *)calloc(1, sizeof(web_object_t));
+
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  listenfd = Open_listenfd(argv[1]); // 전달받은 포트 번호를 사용해서 수신 소켓을 생성한다.
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    clientfd = Malloc(sizeof(int));
+    *clientfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 요청을 수신한다.
+    Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", client_hostname, client_port);
+    Pthread_create(&tid, NULL, thread, clientfd); // Concurrent Proxy
+  }
+
   return 0;
+}
+
+void *thread(void *vargp)
+{
+  int clientfd = *((int *)vargp);
+  Pthread_detach(pthread_self());
+  Free(vargp);
+  doit(clientfd);
+  Close(clientfd);
+  return NULL;
 }
