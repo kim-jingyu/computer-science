c5a25950ca1c34aa6fa8e5e5234bf112ddcbd1bf
kim-jingyu <swc05161@gmail.com>
2023-11-22 14:31:55 +0000

    feat: 클라이언트 요청 수신 기능 구현

--- proxy.c
+++ proxy.c
@@ -16,7 +16,7 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 /* 테스트 환경에 따른 도메인,포트 번호 지정을 위한 상수이다. (0 할당시에 도메인,포트 번호가 고정되어서 외부에서 접속이 가능하다.) */
-static const int in_local_test = 1;
+static const int is_local_test = 1;
 
 int main(int argc, char **argv)
 {
@@ -57,4 +57,93 @@ void *thread(void *vargp)
   doit(clientfd);
   Close(clientfd);
   return NULL;
+}
+
+void doit(int clientfd)
+{
+  int serverfd, content_length;
+  char request_buf[MAXLINE], response_buf[MAXLINE];
+  char method[MAXLINE], uri[MAXLINE], path[MAXLINE], hostname[MAXLINE], port[MAXLINE];
+  char *response_ptr, filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t request_rio, response_rio;
+
+  /* 클라이언트의 요청을 수신한다. */
+
+  // Request Line 읽기 [Client -> Proxy]
+  Rio_readinitb(&request_rio, clientfd);
+  Rio_readlineb(&request_rio, request_buf, MAXLINE);
+  printf("Request headers:\n %s\n", request_buf);
+
+  // 요청 라인 parsing을 통해서 'method, uri, hostname, port, path'를 찾는다.
+  sscanf(request_buf, "%s %s", method, uri);
+  parse_uri(uri, hostname, port, path);
+
+  // Server에 전송하기 위해 요청 라인의 형식을 변경한다. 'method uri version' -> 'method path HTTP/1.0'
+  sprintf(request_buf, "%s %s %s\r\n", method, path, "HTTP/1.0");
+
+  // 지원하지 않는 method인 경우에는 예외 처리한다.
+  if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD"))
+  {
+    clienterror(clientfd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+}
+
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  // HTTP response body
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body>\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>Tiny Web Server</em>\r\n", body);
+
+  // print HTTP response
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+
+  // error msg와 response body를 server socket을 통해 클라이언트에 보낸다.
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
+
+/*
+  uri의 형태가 http://hostname:port/path 혹은 http://hostname/path 일때,
+  uri를 hostname, port, path로 파싱하는 함수이다.
+*/
+void parse_uri(char *uri, char *hostname, char *port, char *path)
+{
+  // hostname의 시작 위치 포인터는 '//'가 있으면 // 뒤 (ptr+2)부터 시작이고, 없으면 uri부터 시작이다.
+  char *hostname_ptr = strstr(uri, "//") ? strstr(uri, "//") + 2 : uri;
+  // port number의 시작 위치
+  char *port_ptr = strchr(hostname_ptr, ':');
+  // path 시작 위치
+  char *path_ptr = strchr(hostname_ptr, '/');
+
+  // port가 있는 경우
+  if (port_ptr)
+  {
+    strncpy(port, port_ptr + 1, path_ptr - port_ptr - 1);
+    strncpy(hostname, hostname_ptr, port_ptr - hostname_ptr);
+  }
+  // port가 없는 경우
+  else
+  {
+    if (is_local_test)
+    {
+      // port의 기본 값인 80으로 포트 번호를 설정한다.
+      strcpy(port, "80");
+    }
+    else
+    {
+      strcpy(port, "8000");
+    }
+
+    strncpy(hostname, hostname_ptr, path_ptr - hostname_ptr);
+  }
 }
